d716081df042d4658148cc2c85708db58468bcbe
Kelo <jonathan.kelo@getadigital.com>
2023-05-02 10:10:40 +0200

    Add queries
    Added query models to capture coinbase response.

---
+++ Coinbase/Coinbase.Commerce.Models/Models/Queries/BaseQueryOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Coinbase.Commerce.Models.Models.Queries;
+
+public class BaseQueryOptions
+{
+    public int? Limit { get; set; } = 25;
+    public string? StartingAfter { get; set; }
+    public string? EndingBefore { get; set; }
+    public string? Order { get; set; } = "desc";
+}
