5ef4d48511621f06835bd35bee0e4f31c7493407
Kelo <jonathan.kelo@getadigital.com>
2023-05-03 09:12:07 +0200

    Create clients for each transaction type
    Divided the clients for the sake of having smaller, more focused and more manageable components.

---
+++ Coinbase/Coinbase.Commerce.Clients/Interfaces/Events/ICoinbaseCommerceEventClient.cs
@@ -0,0 +1,8 @@
+﻿using Coinbase.Commerce.Models.Models.Events;
+using Refit;
+
+namespace Coinbase.Commerce.Clients.Interfaces.Events;
+
+public interface ICoinbaseCommerceEventClient : ICoinbaseCommerceApi<ApiResponse<CoinbaseCommerceEventResponse>>
+{
+}
