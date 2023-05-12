472b70d8d66f5594f5324f684058feaab47379e7
Kelo <jonathan.kelo@getadigital.com>
2023-05-12 11:52:32 +0200

    Add authentication handler with settings
    Added a handler to apply auth key and other important headers on every request made.

---
+++ Coinbase/Coinbase.Commerce.Models/Models/Settings/ApiSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Coinbase.Commerce.Models.Models.Settings;
+
+public class ApiSettings
+{
+    public string ApiKey { get; set; }
+
+    public string ApiVersion { get; set; }
+
+    public string ApiBaseUrl { get; set; }
+}
