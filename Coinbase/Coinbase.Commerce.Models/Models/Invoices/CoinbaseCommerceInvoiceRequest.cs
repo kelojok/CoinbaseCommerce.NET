c38036c3982dceb7191c89e7937244ede0b4662b
Kelo <jonathan.kelo@getadigital.com>
2023-05-13 14:23:35 +0200

    Clean up models
    Removed attributes and re-structured the props to make it easier to read.

--- Coinbase/Coinbase.Commerce.Models/Models/Invoices/CoinbaseCommerceInvoiceRequest.cs
+++ Coinbase/Coinbase.Commerce.Models/Models/Invoices/CoinbaseCommerceInvoiceRequest.cs
@@ -4,14 +4,13 @@ using Newtonsoft.Json;
 namespace Coinbase.Commerce.Models.Models.Invoices;
 
 public record CoinbaseCommerceInvoiceRequest(
-    [property: JsonProperty("business_name")] [property: JsonPropertyName("business_name")]
-    string BusinessName,
-    [property: JsonProperty("customer_email")] [property: JsonPropertyName("customer_email")]
-    string CustomerEmail,
-    [property: JsonProperty("customer_name")] [property: JsonPropertyName("customer_name")]
-    string CustomerName,
-    [property: JsonProperty("memo")] [property: JsonPropertyName("memo")]
-    string Memo,
-    [property: JsonProperty("local_price")] [property: JsonPropertyName("local_price")]
-    LocalPrice LocalPrice
+    [property: JsonPropertyName("business_name")] string BusinessName,
+
+    [property: JsonPropertyName("customer_email")] string CustomerEmail,
+
+    [property: JsonPropertyName("customer_name")] string CustomerName,
+
+    [property: JsonPropertyName("memo")] string Memo,
+
+    [property: JsonPropertyName("local_price")] LocalPrice LocalPrice
 );
